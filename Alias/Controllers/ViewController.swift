d1d745b6e1bb20c241a08d4d565218171576df3a
Ozenforwin <dyadichev.biz@gmail.com>
2022-07-27 12:11:45 +0200

    Initial commit

---
+++ Alias/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alias
+//
+//  Created by Dyadichev on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        
+    }
+}
+
