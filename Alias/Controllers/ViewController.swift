6614e5c6b7292c65e2600e57120d67ef3da37329
Daniel <neskdan812@gmail.com>
2022-07-27 16:54:16 +0300

    fixed folders

--- Alias/Controllers/ViewController.swift
+++ Alias/Controllers/ViewController.swift
@@ -14,16 +14,16 @@ class ViewController: UIViewController {
         view.backgroundColor = .systemBlue
         
         //MARK: - это для проверки как работает
-//        let image = UIImageView()
-//        image.image = UIImage(named: "Image")
-//        let button: UIButton = .createButton(title: "Bla bla", height: 96, color: .systemGray3, image: image)
-//        view.addSubview(button)
-//        button.addSubview(image)
-//        NSLayoutConstraint.activate([
-//            button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 300),
-//            button.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-//            button.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, constant: -40)
-//        ])
+        let image = UIImageView()
+        image.image = UIImage(named: "Image")
+        let button: UIButton = .createButton(title: "Bla bla", height: 96, color: .systemGray3, image: image)
+        view.addSubview(button)
+        button.addSubview(image)
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 300),
+            button.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            button.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, constant: -40)
+        ])
     }
 }
 
